f3ee31abab64c34ce78fecf47b3460c07e631405
Mohammed Manseur <mmanseurdz@hotmail.com>
2023-02-04 10:31:52 -0500

    initialisation server expressjs

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const exphbs= require("express-handlebars");
+const bodyParser = require("body-parser");
+const path = require("path");
+const db = require("./config/database")
+
+ db.authenticate().then(()=>{
+        console.log('Connection has been established successfully.');
+     }). catch (error=>{  
+    console.error('Unable to connect to the database:', error)});
+ 
+const app = express();
+app.get("/", function(req, res) {
+    res.send("Accueil");
+})
+
+//Gig routes
+app.use('/gigs',require('./routes/gigs'));
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server listen to port ${PORT}`));
